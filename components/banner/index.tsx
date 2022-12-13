fe1791c1d0057fc8156e243cd6928c30ade9ceae
Shane Keney <shane.keney@curaleaf.com>
2022-12-13 11:06:48 -0500

    Banner component and basic styles

---
+++ components/banner/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./Banner.module.css";
+
+interface BannerProps {
+  buttonText: string;
+  handleOnClick: () => void;
+}
+
+export default function Banner(props: BannerProps) {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>Coffee </span>
+        <span className={styles.title2}>Connoisseur</span>
+      </h1>
+      <p className={styles.subtitle}>Discover your local coffee shops!</p>
+      <button className={styles.button} onClick={props.handleOnClick}>
+        {props.buttonText}
+      </button>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
+import Banner from "../components/banner";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const handleOnBannerBtnClick = () => {
+    console.log("banner clicked");
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,9 +18,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Coffee Connoisseur</h1>
+        <Banner
+          buttonText="View stores nearby"
+          handleOnClick={handleOnBannerBtnClick}
+        />
       </main>
-
-      <footer className={styles.footer}></footer>
     </div>
   );
 }
